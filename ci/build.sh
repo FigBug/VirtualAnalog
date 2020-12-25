9f90f151018bd56645150a216ba469bf9ae8858c
Roland Rabien <figbug@gmail.com>
2020-12-25 08:11:17 -0800

    Remove setting up fake display

--- ci/build.sh
+++ ci/build.sh
@@ -8,10 +8,6 @@ if [ $OS = "linux" ]; then
 
   sudo apt-get update
   sudo apt-get install clang git ladspa-sdk freeglut3-dev g++ libasound2-dev libcurl4-openssl-dev libfreetype6-dev libjack-jackd2-dev libx11-dev libxcomposite-dev libxcursor-dev libxinerama-dev libxrandr-dev mesa-common-dev webkit2gtk-4.0 juce-tools xvfb
-
-  Xvfb :99 &
-  export DISPLAY=:99
-  sleep 5
 fi
 
 # mac specific stuff
